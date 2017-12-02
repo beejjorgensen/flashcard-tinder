231d3c5f141ff953277565c19cd7f57a7fc46f48
Colombianito66 <colombianito66@gmail.com>
2017-12-01 22:43:11 -0500

    add CSS to modal

--- src/components/SignInModal.js
+++ src/components/SignInModal.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
 import { Button, Header, Image, Modal } from 'semantic-ui-react'
+import '../App.css'
+
 
 class SignInModal extends Component {
   render() {
     return (
-  <Modal trigger={<Button>Sign In</Button>}>
-    <Modal.Header>All the study resources you need to ace the dev interview, in one place.</Modal.Header>
-    <Modal.Content image>
-      <Image wrapped size='medium' src='../assets/study-buddy.jpg' />
-      <Modal.Description>
-        <Header>GITHUB</Header>
-        <Header>GOOGLE +</Header>
-        <Header>FACEBOOK</Header>
-        <Header>SIGN UP</Header>
+      <Modal className="modal" trigger={<Button>Sign In</Button>}>
+    <Modal.Header > Ace the Dev Interview! </Modal.Header>
+    <Modal.Header > All the study resources you'll ever need, in one place. </Modal.Header>
+
+    <Modal.Content  image>
+      <Modal.Description > 
+      <Image wrapped size='medium' src='assets/study-buddy.jpg' />
+        <Header className="modal">GITHUB</Header>
+        <Header >GOOGLE +</Header>
+        <Header >FACEBOOK</Header>
+        <Header >SIGN UP</Header>
         
       </Modal.Description>
     </Modal.Content>
