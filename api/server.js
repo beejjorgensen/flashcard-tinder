8cfba79197f77d7d08ae3867d8cf1ddfaae2150a
Colombianito66 <colombianito66@gmail.com>
2017-12-05 16:49:39 -0500

    complete server + GET req

--- api/server.js
+++ api/server.js
@@ -3,3 +3,23 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 
+const server = express();
+
+server.get('/cards', (req, res) => {
+  console.log('Hello from server.get /cards route!');
+  Card.find({}, (err, post) => {
+    console.log(card);
+    if (err) return res.send(err);
+    res.json(post);
+  });
+});
+
+mongoose.connect('mongodb://localhost/cardsTest', {useMongoClient: true}, (err) => {
+  if (err) return console.log(err);
+  console.log('Connected to FlashCards DataBase!');
+});
+
+server.listen(8080, (err) => {
+  if (err) return console.log('ERROR port 8080');
+ console.log('FlashCard Server Listening On Port 8080');
+});
