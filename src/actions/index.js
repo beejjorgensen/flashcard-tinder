eaa7c8f3eebd4eaf0654caa8631e8750464b296c
John Wells <molekewl2017@gmail.com>
2017-12-06 15:14:29 -0800

    fix action logic

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const FETCH_FLASHCARDS = 'fetch_flashcards';
 
-ROOT_URL = 'http://serverNameHere.herokuapp.com/'
+const ROOT_URL = 'http://serverNameHere.herokuapp.com/'
 
 export function fetchCards() {
   const request = axios.get(`${ROOT_URL}/flashpage`)
