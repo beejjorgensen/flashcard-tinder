1afc934d5dbe830630fb304f0e3b79f6665e238b
Latoyya Smith <latoyyas@gmail.com>
2017-12-06 18:32:06 -0600

    updated assets

--- src/components/Language.js
+++ src/components/Language.js
@@ -6,13 +6,22 @@ class Language extends Component {
     render() {
         return (
             <div>
+            <div className="lang-header"> <h1> Choose a language and start learning now! </h1> </div>
 
     <div className="topic-container">
     <Image.Group size='small'>
-      <a className='btnTopic' href="/javascript"><Image src='/assets/javascript.png' className='topics' /></a>
-      <Image src='/assets/python.png' className='topics' />
-      <Image src='/assets/html-5.png' className='topics' />
-      <Image src='/assets/css3.png' className='topics' />
+      <a className='btnTopic' href="/javascript"><Image src='/assets/javascript_icon.png' className='topics' /></a>
+      <Image src='/assets/python_icon.png' className='topics' />
+      <Image src='/assets/html-5_icon.png' className='topics' />
+      <Image src='/assets/css3_icon.png' className='topics' />
+    </Image.Group>
+    <Divider hidden />
+
+    <Image.Group size='small'>
+      <a className='btnTopic' href="/javascript"><Image src='/assets/javascript_icon.png' className='topics' /></a>
+      <Image src='/assets/python_icon.png' className='topics' />
+      <Image src='/assets/html-5_icon.png' className='topics' />
+      <Image src='/assets/css3_icon.png' className='topics' />
     </Image.Group>
     <Divider hidden />
 
