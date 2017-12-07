c127588331eb4cd3601b702235e4b82dd589a061
Latoyya Smith <latoyyas@gmail.com>
2017-12-07 08:54:15 -0600

    assets update, code clean-up, favicon

--- src/components/Language.js
+++ src/components/Language.js
@@ -4,30 +4,32 @@ import '../App.css'
 
 class Language extends Component {
     render() {
-        return (
-            <div>
-            <div className="lang-header"> <h1> Choose a language and start learning now! </h1> </div>
+    	return (
+				<div>
+				  <div className="typewriter-container">
+						<div className="typewriter">
+							<h1>Choose a language and start learning now!</h1>
+						</div>
+					</div>
+            <div className="lang-container">
+              <Image.Group size='small'>
+                <a className='btnTopic' href="/javascript"><Image src='/assets/javascript_icon.png'/></a>
+                  <Image src='/assets/python_icon.png'/>
+                  <Image src='/assets/html-5_icon.png'/>
+                  <Image src='/assets/css3_icon.png'/>
+              </Image.Group>
+              <Divider hidden />
 
-    <div className="topic-container">
-    <Image.Group size='small'>
-      <a className='btnTopic' href="/javascript"><Image src='/assets/javascript_icon.png' className='topics' /></a>
-      <Image src='/assets/python_icon.png' className='topics' />
-      <Image src='/assets/html-5_icon.png' className='topics' />
-      <Image src='/assets/css3_icon.png' className='topics' />
-    </Image.Group>
-    <Divider hidden />
-
-    <Image.Group size='small'>
-      <a className='btnTopic' href="/javascript"><Image src='/assets/javascript_icon.png' className='topics' /></a>
-      <Image src='/assets/python_icon.png' className='topics' />
-      <Image src='/assets/html-5_icon.png' className='topics' />
-      <Image src='/assets/css3_icon.png' className='topics' />
-    </Image.Group>
-    <Divider hidden />
-
-  </div>
-  </div>
-        );
+              <Image.Group size='small'>
+                  <Image src='/assets/react_icon.png'/>
+                  <Image src='/assets/ruby_on_rails_icon.png'/>
+                  <Image src='/assets/php_icon.png'/>
+                  <Image src='/assets/jquery_icon.png'/>
+              </Image.Group>
+              <Divider hidden />
+						</div>
+  			</div>
+      );
     }
 }
 export default Language
