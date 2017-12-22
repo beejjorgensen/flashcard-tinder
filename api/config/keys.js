dbb0ca7517e3f85ba8386eba3c38ef063b2621fb
Colombianito66 <colombianito66@gmail.com>
2017-12-22 15:08:03 -0500

    update .gitignore

---
+++ api/config/keys.js
@@ -0,0 +1,15 @@
+// add this file to .gitignore
+
+module.exports = {
+  google: {
+    clientID: '652304711010-jgov8fa1h48oc90us5mlbc8krdchhs4q.apps.googleusercontent.com',
+    clientSecret: 'guZrNHg-rLtingc-hbCBbPle'
+  },
+  mongodb: {
+    dbURI:'mongodb://localhost/cardsTest' // switch to mLab for production 
+    // dbURI: process.env.MONGODB_URI/heroku_j6hzksz4
+  },
+  session: {
+    cookieKey:'Lambda1#'
+  }
+}; 
