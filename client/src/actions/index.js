be12b340a5ac7cbab01337f2cd87c47975527088
wlharvey4 <pinecone062@gmail.com>
2018-01-14 20:45:24 -0800

    Added 2nd proxy; removed cors; removed ROOT_URL; fixed faulty route

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,11 +3,9 @@ import axios from 'axios';
 export const FETCH_FLASHCARDS = 'fetch_flashcards';
 export const FETCH_USER = 'fetch_user';
 
-const ROOT_URL = 'http://localhost:5000';
-
 export const fetchCards = () => {
   return (dispatch) => {
-    axios.get(`${ROOT_URL}/api/cards`)
+    axios.get('/api/cards')
     .then((data) => {
       dispatch ({
         type: FETCH_FLASHCARDS,
@@ -18,12 +16,9 @@ export const fetchCards = () => {
 }
 
 export const fetchUser = () => async dispatch => {
-  const res = await axios.get(`${ROOT_URL}/api/current_user`, {
-    withCredentials: true
- });
+  const res = await axios.get('/api/current_user');
   console.log('RES INSIDE ACTIONS.JS', res);
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
-
 
