0f92496397efef15622c82b338478faea6a8c897
Ryan Hamblin <ryanhca@gmail.com>
2018-01-17 18:27:37 -0700

    fixed render issues on currentCard Randomizer

--- client/src/containers/FlashPage.js
+++ client/src/containers/FlashPage.js
@@ -10,83 +10,70 @@ import '../style.css';
 class FlashPage extends Component {
   constructor(props) {
     super(props);
-
-    // this.state = {
-    //   cards: [],
-    //  currentCard: {}
-    // };
-
-    this.updateCard = this.updateCard.bind(this);
+    this.state = {
+      currentCard: {}
+    };
   }
 
-  // componentWillMount() {
-  //   const currentCards = this.props.cards;
-  //   console.log(">>>>>>>>CWM CURRENTCARDS", currentCards);
-    
-
-  //   this.setState({
-  //     cards: currentCards,
-  //     currentCard: this.getRandomCard(currentCards)
-  //   })
-  //   console.log('CurrentCard', currentCards);
-  // }
-
-  getRandomCard(currentCards) {
-    const card = currentCards[Math.floor(Math.random() * currentCards.length)]
-    // console.log('GET RANDOM CARD', card);
-    return(card);
+  componentWillReceiveProps(nextProps) {
+    if (this.props.cards[0] !== nextProps.cards[0]) {
+      const currentCard =
+        nextProps.cards[Math.floor(Math.random() * nextProps.cards.length)];
+      this.setState(prevState => {
+        return {
+          currentCard
+        };
+      });
+    }
   }
 
-  updateCard() {
-    const currentCards = this.props.cards;
-    // console.log('UPDATECARDS : >>>', currentCards);
-    this.setState({
-      currentCard: this.getRandomCard(currentCards)
-    })
-  
-  }
-  // console.log('CURRENT CARD', currentCard);
-  
-  
   render() {
     // console.log('CURRENT CARD', this.state.currentCard);
     // let x = this.props.cards[5];
     // console.log('BEEJ PROPS', this.props);
     // console.log('CARDS PLEASE', JSON.stringify(x, null, 4));
     // console.log('>>>CARDSPLZ 2', typeof(x));
+    const { currentCard } = this.state;
     return (
       <div className="App">
         <FixedMenu inverted />
-        <div className='cardRow'>
-          <FlashCard front={this.props.cards[5]}
-                     back={this.props.cards[5]} 
-          />
+        <div className="cardRow">
+          <FlashCard front={currentCard} back={currentCard} />
         </div>
-        <div className='buttonRow'>
+        <div className="buttonRow">
           <DrawButton drawCard={this.updateCard} />
         </div>
-        <Popup trigger={<Icon name='info circle' size='large' inverted style={ { padding: '0 0 0 0', margin: '0 auto' } }/>} flowing hoverable>
-            <Grid centered divided columns={3}>
-              <Grid.Column textAlign='center'>
-                <p>Red Button removes card from consideration</p>
-              </Grid.Column>
-              <Grid.Column textAlign='center'>
-                <p>Yellow Button keeps card at same pace</p>
-              </Grid.Column>
-              <Grid.Column textAlign='center'>
-                <p>Green Button shows card more frequently</p>
-              </Grid.Column>
-            </Grid>
-          </Popup>
+        <Popup
+          trigger={
+            <Icon
+              name="info circle"
+              size="large"
+              inverted
+              style={{ padding: '0 0 0 0', margin: '0 auto' }}
+            />
+          }
+          flowing
+          hoverable
+        >
+          <Grid centered divided columns={3}>
+            <Grid.Column textAlign="center">
+              <p>Red Button removes card from consideration</p>
+            </Grid.Column>
+            <Grid.Column textAlign="center">
+              <p>Yellow Button keeps card at same pace</p>
+            </Grid.Column>
+            <Grid.Column textAlign="center">
+              <p>Green Button shows card more frequently</p>
+            </Grid.Column>
+          </Grid>
+        </Popup>
       </div>
-      
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   console.log('FlashPage MAPSTATETOPROPS', state);
-  return {cards: state.cards}
-}
+  return { cards: state.cards };
+};
 export default connect(mapStateToProps)(FlashPage);
-
